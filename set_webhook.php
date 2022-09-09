5a9943d70b4f2cdb86f967de4f4cb0d21905350f
yaroslavrib <yaroslavrib@gmail.com>
2022-09-09 21:54:33 +0300

    Add files via upload

---
+++ set_webhook.php
@@ -0,0 +1,21 @@
+<?php
+	$auth_token = '4fc524fbaca7e64d-b7b61d79fd4186af-46eac8a604fbcf96';
+	$webhook = 'https://github.com/yaroslavrib/ViberBot.github.io';
+	
+	$jsonData = 
+	'{
+		"auth_token": "'.$auth_token.'",
+		"url": "'.$webhook.'",
+		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
+	}';
+	
+	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
+	if($err) {echo($err);}
+	else {echo($response);}
+?>
